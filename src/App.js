60a79038a26e9697cec72902bc5b35b13124ee86
mbiradar55 <38130738+mbiradar55@users.noreply.github.com>
2022-07-21 08:30:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import FootballMatches from './components/football';
+import './styles.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <FootballMatches />
+    </div>
+  );
+}
